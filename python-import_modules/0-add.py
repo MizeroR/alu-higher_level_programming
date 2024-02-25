7d09eb91bbe9b80dcdbf395194364866848e95de
MizeroR <156176469+MizeroR@users.noreply.github.com>
2024-02-25 18:57:17 +0200

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     a = 1
     b = 2
     addition = a + b
-    print( $a " + " $b = addition.format(a, b, addition)
+    print( "$a " + " $b = "$addition.format(a, b, addition)
