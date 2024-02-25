345c5a2e0e885e133f0720c03e1f1ed7bd23f0d3
MizeroR <156176469+MizeroR@users.noreply.github.com>
2024-02-25 19:23:13 +0200

    Update 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-count = len(argv) - 1
+count = len(argv)
 if count == 0:
-   print("0 arguments.")
+   print("0 arguments.".format(count -1))
 elif count == 1:
-    print("1 argument:")
+    print("1 argument:".format(count -1))
 else:
-    print("{} arguments:".format(count))
+    print("{} arguments:".format(count -1))
 for i in range(count):
     print("{}: {}".format(i + 1, argv[i + 1]))
