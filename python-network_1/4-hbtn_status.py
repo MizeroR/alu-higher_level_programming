29632f34c7ac0a2fedf56da8771614d136e20603
MizeroR <r.mizero@alustudent.com>
2024-03-25 16:33:41 +0000

    changes

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -3,9 +3,9 @@
 
 import requests
 
-url = 'https://alu-intranet.hbtn.io/status'
+url = 'https://intranet.hbtn.io/status'
 response = requests.get(url)
 
 print("Body response:")
-print("\t- type:", type(response.text).__name__)
-print("\t- content:", response.text)
+print("\t- type:{}", format(type(response.text).__name__))
+print("\t- content:{}", format(response.text))
