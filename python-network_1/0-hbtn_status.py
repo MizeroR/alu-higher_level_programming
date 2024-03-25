e1708711d8970197b2e0165c56f00068a8f87d19
MizeroR <r.mizero@alustudent.com>
2024-03-25 17:21:55 +0000

    changes

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python3
-'''a Python script that fetches a url'''
-
-
+#!usr/bin/python3
 import urllib.request
 
+url = "https://intranet.hbtn.io/status"
 
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
+try:
+    with urllib.request.urlopen(url) as response:
+        data = response.read().decode('utf-8')
+
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+except Exception as e:
+    print("Error fetching URL:", e)
