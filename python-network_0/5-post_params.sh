cb9123b582c2b68d7b2fa38c505ecf6b03911fcb
MizeroR <r.mizero@alustudent.com>
2024-03-26 09:52:21 +0000

    changes

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Curl sends POST req to URL, display response body
-curl -s -X POST -d "test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
