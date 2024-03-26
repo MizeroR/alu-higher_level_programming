0e9c5bdc0f668a93d55adb14eef5724e197aa2ff
MizeroR <r.mizero@alustudent.com>
2024-03-26 10:30:22 +0000

    changes

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,13 +6,13 @@ import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file')\
-    .load_from_json_file
+    load_from_json_file = __import__('6-load_from_json_file') \
+        .load_from_json_file
 
     try:
         loadFile = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        loadFile = []
+        loadFile = ["Holberton"]
 
     argc = len(sys.argv)
     for idx in range(1, argc):
