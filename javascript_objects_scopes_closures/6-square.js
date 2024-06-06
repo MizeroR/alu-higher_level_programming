809767caa54ae947c66c881c388045fc0990fab7
MizeroR <r.mizero@alustudent.com>
2024-06-06 02:55:27 -0700

    chnages

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,5 +1,5 @@
- 
-rts = class Square extends require('./5-square.js') {
+#!/usr/bin/node
+module.exports = class Square extends require('./5-square.js') {
 	  charPrint (c) {
 		      if (c === undefined) {
 			            this.print();
